012ddac2e1ecba6318b1226d215b57e59ccddfee
pumpk <cong2738@gmail.com>
2025-03-10 15:06:14 +0900

    100hz생성기, 타임카운터 모듈 생성, 스탑워치 데이터패스 모듈 작성

--- 250310stop_watch_FSM/250310stop_watch_FSM.srcs/sources_1/new/stopwatch_dp.v
+++ 250310stop_watch_FSM/250310stop_watch_FSM.srcs/sources_1/new/stopwatch_dp.v
@@ -11,37 +11,138 @@ module stopwatch_dp (
     output [6:0] hour
 );
 
-clk_divider #(
-    .FCOUNT(1_000_000)
-) (
-    .clk(),
-    .reset(),
-    .o_clk()
-);
+    wire w_100hz;
+    tick_generator100hz #(
+        .FCOUNT(1_000_000)
+    ) Tick_Generator100hz (
+        .clk(clk),
+        .reset(reset),
+        .o_clk(w_100hz) // period:0.01sec = 10msec
+    );
+
+    wire w_msec, w_1hz;
+    time_counter #(
+        .TICK_COUNT(100),
+        .BIT_WIDTH(7)
+    ) msec_counter(
+        .clk(clk),
+        .reset(reset),
+        .tick(w_100hz),
+        .count(w_msec),
+        .o_tick(w_1hz) // period:1sec
+    );
+
+    wire w_sec, w_sec_fq;
+    time_counter #(
+        .TICK_COUNT(60),
+        .BIT_WIDTH(7)
+    ) sec_counter(
+        .clk(clk),
+        .reset(reset),
+        .tick(w_1hz),
+        .count(w_sec),
+        .o_tick(w_sec_fq) // period:1min
+    );
+
+    wire w_min, w_min_fq;
+    time_counter #(
+        .TICK_COUNT(60),
+        .BIT_WIDTH(7)
+    ) min_counter(
+        .clk(clk),
+        .reset(reset),
+        .tick(w_sec_fq),
+        .count(w_min),
+        .o_tick(w_min_fq) // period:1hour
+    );
+
+    wire w_hour, w_hour_fq;
+    time_counter #(
+        .TICK_COUNT(24),
+        .BIT_WIDTH(7)
+    ) hour_counter(
+        .clk(clk),
+        .reset(reset),
+        .tick(w_min_fq),
+        .count(w_hour),
+        .o_tick(w_hour_fq) // period:1day
+    );
+
+    assign msec = w_msec;
+    assign sec = w_sec;
+    assign min = w_min;
+    assign hour = w_hour;
 
 endmodule
 
-module clk_divider #(
+
+module time_counter #(
+    parameter TICK_COUNT = 100,
+    parameter BIT_WIDTH = 7
+) (
+    input clk,
+    input reset,
+    input tick,
+    input count,
+    input o_tick
+);
+    reg [$clog2(TICK_COUNT):0] count_reg, count_next;
+    reg tick_reg, tick_next;
+
+    always @(posedge clk, posedge reset) begin
+        if(reset) begin
+            count_reg <= 0;
+            tick_reg <= 0;
+        end else begin
+            count_reg = count_next;
+            tick_reg = tick_next;
+        end
+    end
+
+    always @(*) begin
+        if(count_next == TICK_COUNT - 1) begin
+            count_next = 0;
+            tick_next = 1;
+        end else begin
+            count_next = count_next + 1;
+            tick_next = 0;
+        end
+    end
+
+    assign count = count_reg;
+    assign o_tick = tick_reg;
+endmodule
+
+module tick_generator100hz #(
     parameter FCOUNT = 1_000_000
 ) (
     input  clk,
     input  reset,
+    input run,
+    input clear,
     output o_clk
 );
     reg [$clog2(FCOUNT)-1:0] count_reg, count_next;
     reg clk_reg, clk_next;  //출력을 f/f으로 내보내기 위해 사용
 
     always @(posedge clk, posedge reset) begin
-        if (reset) begin
+        if(clear) begin
             count_next <= 0;
-            clk_reg <= 0;
-        end else begin
-            count_reg <= count_next;
-            clk_reg   <= clk_next;
+            clk_next <= 0;
+        end else if(run) begin
+            if (reset) begin
+                count_reg <= 0;
+                clk_reg <= 0;
+            end else begin
+                count_reg <= count_next;
+                clk_reg   <= clk_next;
+            end    
         end
     end
 
     always @(*) begin
+        count_next = count_reg;
+        clk_next = clk_reg;
         if (count_next == FCOUNT - 1) begin
             count_next = 0;
             clk_next   = 1'b1;

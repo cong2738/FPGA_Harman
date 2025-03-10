34b361b25280741477ee430e020ea950b181e9ea
pumpk <cong2738@gmail.com>
2025-03-10 14:08:54 +0900

    클록디바이더 테스트벤치 생성

--- 250310stop_watch_FSM/250310stop_watch_FSM.srcs/sim_1/new/tb_clk_divider.v
+++ 250310stop_watch_FSM/250310stop_watch_FSM.srcs/sim_1/new/tb_clk_divider.v
@@ -1,26 +1,6 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2025/03/10 14:07:40
-// Design Name: 
-// Module Name: tb_clk_divider
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module tb_clk_divider ();
+    
 
-module tb_clk_divider(
-
-    );
 endmodule

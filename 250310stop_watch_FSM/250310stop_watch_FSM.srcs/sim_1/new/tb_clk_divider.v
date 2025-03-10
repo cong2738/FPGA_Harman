7366bd131db85eb546a20ea2ea84b05e8564eebb
pumpk <cong2738@gmail.com>
2025-03-10 15:05:32 +0900

    클록디바이더 테스트벤치 생성

--- 250310stop_watch_FSM/250310stop_watch_FSM.srcs/sim_1/new/tb_clk_divider.v
+++ 250310stop_watch_FSM/250310stop_watch_FSM.srcs/sim_1/new/tb_clk_divider.v
@@ -1,6 +1,30 @@
 `timescale 1ns / 1ps
 
 module tb_clk_divider ();
-    
+    reg clk, reset, run, clear;
+    wire o_clk;
+
+    clk_divider #(
+        .FCOUNT(1_000_000)
+    ) DUT (
+        .clk  (clk),
+        .reset(reset),
+        .run  (run),
+        .clear(clear),
+        .o_clk(o_clk)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk   = 0;
+        reset = 1;
+        run   = 1;
+        clear = 0;
+        #10 reset = 0;
+        #100; run = 0;
+        
+
+    end
 
 endmodule

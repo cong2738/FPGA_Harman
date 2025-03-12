ce083969db2aaa03f78de245c1140ffd93f5feec
pumpk <cong2738@gmail.com>
2025-03-12 10:35:23 +0900

    비트스트림 생성

--- 2025-03-12_stopwatch/2025-03-12_stopwatch.runs/synth_1/Top_Upcounter.tcl
+++ 2025-03-12_stopwatch/2025-03-12_stopwatch.runs/synth_1/Top_Upcounter.tcl
@@ -70,6 +70,8 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
+set_param chipscope.maxJobs 4
+set_param xicom.use_bs_reader 1
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7a35tcpg236-1
 
@@ -80,6 +82,7 @@ set_property webtalk.parent_dir C:/Users/kccistc/Desktop/FPGA_Harman/2025-03-12_
 set_property parent.project_path C:/Users/kccistc/Desktop/FPGA_Harman/2025-03-12_stopwatch/2025-03-12_stopwatch.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
+set_property ip_output_repo c:/Users/kccistc/Desktop/FPGA_Harman/2025-03-12_stopwatch/2025-03-12_stopwatch.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }

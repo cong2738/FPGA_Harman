884e9bc04db3cdb7b79a19ce0273fdca3b6955e6
pumpk <cong2738@gmail.com>
2025-03-18 11:20:47 +0900

    비트스트림

--- uart_0318/uart_0318.srcs/sources_1/imports/sources_1/new/send_tx_btn.v
+++ uart_0318/uart_0318.srcs/sources_1/imports/sources_1/new/send_tx_btn.v
@@ -16,17 +16,26 @@ module send_tx_btn #(
         .o_btn(d_start)
     );
 
-    wire w_tx_busy;
+    // wire w_tx_busy;
+    // shot_15tic U_Shot (
+    //     .start_shot(d_start),
+    //     .tx_busy(w_tx_busy),
+    //     .clk(clk),
+    //     .rst(rst),
+    //     .tx_tirgger(uart_start)
+    // );
+
     wire uart_start;
-    shot_15tic U_Shot (
-        .start_shot(d_start),
-        .tx_busy(w_tx_busy),
+    wire [7:0] i_uart_data;
+    shot_15tic U_CU (
         .clk(clk),
         .rst(rst),
-        .tx_tirgger(uart_start)
+        .d_start(d_start),
+        .tx_busy(w_tx_busy),
+        .uart_start(uart_start),
+        .data(i_uart_data)
     );
 
-    wire [7:0] i_uart_data;
     uart #(
         .BAUD_RATE(BAUD_RATE)
     ) U_Uart (
@@ -38,86 +47,79 @@ module send_tx_btn #(
         .tx_busy(w_tx_busy)
     );
 
+endmodule
+
+module shot_15tic (
+    input  clk,
+    input  rst,
+    input  d_start,
+    input  tx_busy,
+    output uart_start,
+    output[7:0] data
+);
     //send tx ascii to PC
     reg [7:0] data_curr, data_next;
+    reg [1:0] state, next;  //send char fsm state
+    reg send_reg, send_next;  //start trigger state
+    reg [3:0] send_count_reg, send_count_next;
 
-    assign i_uart_data = data_curr;
+    assign data = data_curr;
+    assign uart_start  = send_reg;
 
+    localparam IDLE = 0, START = 1, SEND = 2;
     always @(posedge clk, posedge rst) begin
         if (rst) begin
-            data_curr <= "0" - 1;
+            data_curr <= "0";
+            state = IDLE;
+            send_reg = 0;
+            send_count_reg = 0;
         end else begin
             data_curr <= data_next;
+            state = next;
+            send_reg = send_next;
+            send_count_reg = send_count_next;
         end
     end
 
     always @(*) begin
         data_next = data_curr;
-        if (uart_start) begin  // debounsed start triger
-            if (data_next == "z") begin
-                data_next = "0";
-            end else data_next = data_curr + 1;  // increase 1 for ASCII
-        end else begin
-            data_next = data_curr;
-        end
-    end
-
-endmodule
-
-module shot_15tic (
-    input  start_shot,
-    input  tx_busy,
-    input  clk,
-    input  rst,
-    output tx_tirgger
-);
-    localparam STOP = 0, RUN = 1;
-
-    reg state, next;
-    reg cur_trigger, next_trigger;
-    reg [3:0] count_reg, count_next;
-
-    assign tx_tirgger = cur_trigger;
-
-    always @(posedge clk, posedge rst) begin
-        if (rst) begin
-            state <= 0;
-            cur_trigger <= 0;
-            count_reg <= 0;
-        end else begin
-            state = next;
-            cur_trigger = next_trigger;
-            count_reg = count_next;
-        end
-    end
-
-    always @(*) begin
-        count_next = count_reg;
         next = state;
-        next_trigger = 0;
+        send_next = 0;
+        send_count_next = send_count_reg;
         case (state)
-            STOP: begin
-                if (start_shot) begin
-                    next = RUN;
+            IDLE: begin
+                send_next = 0;
+                send_count_next = 0;
+                if (d_start) begin
+                    next = START;
+                    send_next = 1;
                 end
             end
-            RUN: begin
-                if (!tx_busy) begin
-                    next_trigger = 1;
-                    if (count_reg == 15) begin
-                        count_next = 0;
+            START: begin
+                send_next = 0;
+                if (tx_busy) begin
+                    next = SEND;
+                end
+            end
+            SEND: begin
+                if (tx_busy == 0) begin
+                    send_next = 1;
+                    send_count_next = send_count_reg + 1;
+                    if (send_count_reg == 15) begin
+                        next = IDLE;
+                        send_count_next = 0;
                     end else begin
-                        count_next = count_reg + 1;
+                        next = START;
                     end
-                end else if (count_reg == 15) begin
-                    next = STOP;
-                    count_next = 0;
-                end else begin
-                    next_trigger = 0;
-                    count_next = count_reg;
+
+                    if (data_next == "z") begin
+                        data_next = "0";
+                    end else begin
+                        data_next = data_curr + 1;  // increase 1 for ASCII
+                    end
                 end
             end
-            default: ;
+            default: next = IDLE;
         endcase
     end
 endmodule

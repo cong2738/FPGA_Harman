767154082f6cfb90ce01751fd9e186c9427eb377
pumpk <cong2738@gmail.com>
2025-03-19 13:41:24 +0900

    ram 모듈(순차논리회로, 조합논리회로)

---
+++ RAM_0319/RAM_0319.srcs/sim_1/new/tb_register.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/03/19 10:39:36
+// Design Name: 
+// Module Name: tb_register
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_register(
+
+    );
+endmodule

f7584a619ea1eabbd83b266500cc5c60898a214f
pumpk <cong2738@gmail.com>
2025-03-19 14:32:16 +0900

    que 모듈 생성

---
+++ RAM_0319/RAM_0319.srcs/sources_1/new/fifo_que.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/03/19 14:31:45
+// Design Name: 
+// Module Name: fifo_que
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fifo_que(
+
+    );
+endmodule

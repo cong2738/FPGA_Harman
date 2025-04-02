86ea6bca9f06162a056d705676f19685a31aca01
pumpk <cong2738@gmail.com>
2025-04-02 12:09:55 +0900

    카운터 컨트롤유닛(FMS)완성

--- 0402_count10000_NJH/0402_count10000_NJH.srcs/sources_1/imports/Downloads/counter_up_down.v
+++ 0402_count10000_NJH/0402_count10000_NJH.srcs/sources_1/imports/Downloads/counter_up_down.v
@@ -4,16 +4,21 @@ module top_counter_up_down (
     input        clk,
     input        reset,
     input        mode,
+    input        run,
+    input        clear,
     output [3:0] fndCom,
     output [7:0] fndFont
 );
     wire [13:0] fndData;
-    wire[3:0] dot_data;
-    counter_up_down U_Counter (
-        .clk  (clk),
-        .reset(reset),
-        .mode (mode),
-        .count(fndData),
+    wire [ 3:0] dot_data;
+
+    counter_up_down u_counter_up_down (
+        .clk     (clk),
+        .reset   (reset),
+        .mode    (mode),
+        .run     (run),
+        .clear   (clear),
+        .count   (fndData),
         .dot_data(dot_data)
     );
 
@@ -31,10 +36,13 @@ module counter_up_down (
     input         clk,
     input         reset,
     input         mode,
+    input         run,
+    input         clear,
     output [13:0] count,
-    output [3:0] dot_data
+    output [ 3:0] dot_data
 );
     wire tick;
+    wire [$clog2(3)-1:0] count_state;
 
     clk_div_10hz U_Clk_Div_10Hz (
         .clk  (clk),
@@ -42,16 +50,25 @@ module counter_up_down (
         .tick (tick)
     );
 
+    counter_CU u_counter_CU (
+        .clk        (clk),
+        .reset      (reset),
+        .mode       (mode),
+        .run        (run),
+        .clear      (clear),
+        .count_state(count_state)
+    );
+
     counter U_Counter_Up_Down (
-        .clk  (clk),
+        .clk(clk),
         .reset(reset),
-        .tick (tick),
-        .mode (mode),
+        .tick(tick),
+        .count_state(count_state),
         .count(count)
     );
 
     dot_data_ctrl u_dot_data_ctrl (
-        .bcd    (count),
+        .bcd     (count),
         .dot_data(dot_data)
     );
 
@@ -59,12 +76,13 @@ endmodule
 
 
 module counter (
-    input         clk,
-    input         reset,
-    input         tick,
-    input         mode,
-    output [13:0] count
+    input                  clk,
+    input                  reset,
+    input                  tick,
+    input  [$clog2(3)-1:0] count_state,
+    output [         13:0] count
 );
+    localparam STOP = 0, DOWN = 1, UP = 2, CLEAR = 3;
     reg [$clog2(10000)-1:0] counter;
 
     assign count = counter;
@@ -72,8 +90,9 @@ module counter (
     always @(posedge clk, posedge reset) begin
         if (reset) begin
             counter <= 0;
-        end else begin
-            if (mode == 1'b0) begin
+        end else if (count_state != STOP) begin
+            if (count_state == CLEAR) counter <= 0;
+            else if (count_state == UP) begin
                 if (tick) begin
                     if (counter == 9999) begin
                         counter <= 0;
@@ -81,7 +100,7 @@ module counter (
                         counter <= counter + 1;
                     end
                 end
-            end else begin
+            end else if (count_state == DOWN) begin
                 if (tick) begin
                     if (counter == 0) begin
                         counter <= 9999;
@@ -123,4 +142,62 @@ module dot_data_ctrl (
 );
     assign dot_data = (bcd % 10 < 5) ? 4'b1101 : 4'b1111;
 
+endmodule
+
+module counter_CU (
+    input clk,
+    input reset,
+    input mode,
+    input run,
+    input clear,
+    output [$clog2(3)-1:0] count_state
+);
+    localparam STOP = 0, DOWN = 1, UP = 2, CLEAR = 3;
+
+    reg [$clog2(3)-1:0] state, next;
+
+    assign count_state = state;
+
+    always @(posedge clk, posedge reset) begin
+        if (reset) begin
+            state <= 0;
+        end else begin
+            state <= next;
+        end
+    end
+
+    always @(*) begin
+        next = state;
+        case (state)
+            STOP: begin
+                if (clear) next = CLEAR;
+                else if (run) begin
+                    if (mode) next = DOWN;
+                    else next = UP;
+                end
+            end
+
+            UP: begin
+                if (!run) next = STOP;
+                else if (clear) next = CLEAR;
+                else if (mode) next = DOWN;
+            end
+
+            DOWN: begin
+                if (!run) next = STOP;
+                else if (clear) next = CLEAR;
+                else if (!mode) next = UP;
+            end
+
+            CLEAR: begin
+                if (!clear) begin
+                    if (!run) next = STOP;
+                    else begin
+                        if (mode) next = DOWN;
+                        else next = UP;
+                    end
+                end
+            end
+        endcase
+    end
 endmodule

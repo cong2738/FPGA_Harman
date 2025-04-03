57e72f6ef8431c5ad80bf5f40c08fe2ae22f788a
pumpk <cong2738@gmail.com>
2025-04-03 12:45:19 +0900

    tx완성

--- 0402_UART_count10000_NJH/0402_UART_count10000_NJH.srcs/sources_1/new/uart.v
+++ 0402_UART_count10000_NJH/0402_UART_count10000_NJH.srcs/sources_1/new/uart.v
@@ -13,37 +13,219 @@ module uart (
     output rx_done,
     output rx_busy
 );
+    wire tick;
+
+    baudrate_gen U_Baudrate_Gen (
+        .clk  (clk),
+        .reset(reset),
+        .tick (tick)
+    );
+
+    tx U_Transmitter (
+        .clk       (clk),
+        .reset     (reset),
+        .tick      (tick),
+        .tx_trigger(tx_trigger),
+        .tx_data   (tx_data),
+        .tx_done   (tx_done),
+        .tx_busy   (tx_busy),
+        .tx        (tx)
+    );
 
 endmodule
 
 module tx (
     input clk,
     input reset,
+    input tick,
     input tx_trigger,
     input [7:0] tx_data,
     output tx_done,
     output tx_busy,
-    output tx
+    output reg tx
 );
+    localparam IDLE = 0, START = 1, DATA = 2, STOP = 3;
 
+    reg [1:0] state, next;
+    reg [7:0] temp_data, temp_data_next;
+    reg [2:0] bit_count, bit_count_next;
+    reg [3:0] tick_count, tick_count_next;
+    reg done, done_next;
+    reg busy, busy_next;
+
+    assign tx_busy = busy, tx_done = done;
+
+    always @(posedge clk, posedge reset) begin
+        if (reset) begin
+            state      <= IDLE;
+            temp_data  <= 0;
+            tick_count <= 0;
+            bit_count  <= 0;
+            done       <= 0;
+            busy       <= 0;
+        end else begin
+            state      <= next;
+            temp_data  <= temp_data_next;
+            tick_count <= tick_count_next;
+            bit_count  <= bit_count_next;
+            done       <= done_next;
+            busy       <= busy_next;
+        end
+    end
+
+    always @(*) begin
+        next = state;
+        temp_data_next = temp_data;
+        tick_count_next = tick_count;
+        bit_count_next = bit_count;
+        done_next = 0;
+        busy_next = 0;
+
+        case (state)
+            IDLE: begin
+                tx = 1;
+                if (tx_trigger) begin
+                    temp_data_next = tx_data; // ** 데이터를 버퍼에 넣어서 "유지" 시킨다.
+                    next = START;
+                end
+            end
+            START: begin
+                tx = 0;
+                busy_next = 1;
+                if (tick) begin
+                    if (tick_count == 15) begin
+                        tick_count_next = 0;
+                        bit_count_next  = 0;
+                        next            = DATA;
+                    end else begin
+                        tick_count_next = tick_count + 1;
+                    end
+                end
+            end
+            DATA: begin
+                tx = temp_data[0];
+                busy_next = 1;
+                if (tick) begin
+                    if (tick_count == 15) begin
+                        tick_count_next = 0;
+                        if (bit_count == 7) begin
+                            next = STOP;
+                        end else begin
+                            bit_count_next = bit_count + 1;
+                            temp_data_next = {1'b0, temp_data[7:1]};
+                        end
+                    end else begin
+                        tick_count_next = tick_count + 1;
+                    end
+                end
+            end
+            STOP: begin
+                tx = 1;
+                busy_next = 1;
+                if (tick) begin
+                    if (tick_count == 15) begin
+                        tick_count_next = 0;
+                        done_next       = 1;  // 한틱만
+                        next            = IDLE;
+                    end else begin
+                        tick_count_next = tick_count + 1;
+                    end
+                end
+            end
+        endcase
+    end
 endmodule
 
 
 module rx (
     input clk,
     input reset,
+    input tick,
     input rx,
     output [7:0] rx_data,
     output rx_done,
     output rx_busy
 );
+    localparam IDLE = 0, START = 1, DATA = 2, STOP = 3;
+
+    reg [1:0] state, next;
+    reg [2:0] bit_count, bit_count_next;
+    reg [3:0] tick_count, tick_count_next;
+    reg [7:0] data, data_next;
+    reg busy, busy_next;
+    reg done, done_next;
+
+    assign rx_data = data, rx_done = done, rx_busy = busy;
+
+    always @(posedge clk, posedge reset) begin
+        if (reset) begin
+            state <= 0;
+            bit_count <= 0;
+            tick_count <= 0;
+            data <= 0;
+            busy <= 0;
+            done <= 0;
+        end else begin
+            state <= next;
+            bit_count <= bit_count_next;
+            tick_count <= tick_count_next;
+            data <= data_next;
+            busy <= busy_next;
+            done <= done_next;
+        end
+    end
+
+    always @(*) begin
+        next = state;
+        bit_count_next = bit_count;
+        tick_count_next = tick_count;
+        data_next = data;
+        busy_next = 0;
+        done_next = 0;
+        case (state)
+            IDLE: begin
+
+            end
+            START: begin
+
+            end
+            DATA: begin
+
+            end
+            STOP: begin
+
+            end
+        endcase
+    end
 
 endmodule
 
-module baudrate_gen #(parameter BAUDRATE = 9600)(
-    input clk,
-    input reset,
+module baudrate_gen #(
+    parameter BAUDRATE = 9600
+) (
+    input  clk,
+    input  reset,
     output tick
 );
-    
+    localparam COUNT_MAX = 100_000_000 / (BAUDRATE * 16);
+    reg [$clog2(COUNT_MAX)-1:0] r_tick;
+    reg count;
+
+    assign tick = r_tick;
+
+    always @(posedge clk, posedge reset) begin
+        if (reset) begin
+            count  <= 0;
+            r_tick <= 0;
+        end else begin
+            if (count == COUNT_MAX - 1) begin
+                count  <= 0;
+                r_tick <= 1;
+            end else begin
+                count = count + 1;
+                r_tick <= 0;
+            end
+        end
+    end
+
 endmodule

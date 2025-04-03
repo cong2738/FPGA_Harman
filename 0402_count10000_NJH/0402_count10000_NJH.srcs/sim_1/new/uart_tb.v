8437bffdc59a1009513b3e095c0589945d16d5e6
pumpk <cong2738@gmail.com>
2025-04-03 09:28:07 +0900

    쓸모 없는 주석 제거

--- 0402_count10000_NJH/0402_count10000_NJH.srcs/sim_1/new/uart_tb.v
+++ 0402_count10000_NJH/0402_count10000_NJH.srcs/sim_1/new/uart_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2025/04/02 15:36:28
-// Design Name: 
-// Module Name: uart_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module uart_tb ();
     localparam IDLE = 0, WAIT = 1, DATA = 2, STOP = 3;

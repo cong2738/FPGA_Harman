65afe16b652ea7d6c059c10726877003288bde91
pumpk <cong2738@gmail.com>
2025-04-03 09:58:06 +0900

    feat: UART_입출력만 생성

--- 0402_UART_count10000_NJH/0402_UART_count10000_NJH.srcs/sources_1/new/uart.v
+++ 0402_UART_count10000_NJH/0402_UART_count10000_NJH.srcs/sources_1/new/uart.v
@@ -1,12 +1,42 @@
 `timescale 1ns / 1ps
 
-module uart(
+module uart (
     input clk,
     input reset,
     input tx_trigger,
-    input[7:0] tx_data,
+    input [7:0] tx_data,
     input rx,
     output tx,
-    output[7:0] rx_data
-    );
+    output tx_done,
+    output tx_busy,
+    output [7:0] rx_data,
+    output rx_done,
+    output rx_busy
+);
+
+endmodule
+
+module tx (
+    input clk,
+    input reset,
+    input tx_trigger,
+    input tx_trigger,
+    input [7:0] tx_data,
+    output tx_done,
+    output tx_busy,
+    output tx
+);
+
+endmodule
+
+
+module rx (
+    input clk,
+    input reset,
+    input rx,
+    output [7:0] rx_data,
+    output rx_done,
+    output rx_busy
+);
+
 endmodule

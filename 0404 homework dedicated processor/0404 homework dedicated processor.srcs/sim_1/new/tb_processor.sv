633815e93b5234e5b9654319ba8d2969a6a66cf5
pumpk <cong2738@gmail.com>
2025-04-06 11:42:18 +0900

    refactor: sum출력 display에서 monitor로 바꿈

--- 0404 homework dedicated processor/0404 homework dedicated processor.srcs/sim_1/new/tb_processor.sv
+++ 0404 homework dedicated processor/0404 homework dedicated processor.srcs/sim_1/new/tb_processor.sv
@@ -5,12 +5,12 @@ module tb_processor ();
     reg reset;
     wire [7:0] sum_out;
 
-    top_DedicatedProcessor uut (
-        .clk    (clk),
-        .reset  (reset),
-        .sum_out(sum_out)
+    top_DedicatedProcessor uut(
+        .clk     (clk     ),
+        .reset   (reset   ),
+        .sum_out (sum_out )
     );
-
+    
     always #5 clk = ~clk;
 
     initial begin
@@ -18,7 +18,7 @@ module tb_processor ();
         reset = 1;
         #5 reset = 0;
         while (!uut.comp_Aand10) begin
-            @(posedge uut.sum_out) $display("A:%d", uut.sum_out);
+           @(posedge uut.sum_out) $monitor("sum:%d", uut.sum_out); 
         end
         #100 $stop;
     end

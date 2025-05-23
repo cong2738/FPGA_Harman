7b18d637315f4bdf2a2d99879b0f6e6ed209d5d3
pumpk <cong2738@gmail.com>
2025-05-24 04:43:57 +0900

    tb_I2C_Slave: 코드 정리 및 start_transmission, end_transmission 작업 개선

--- I2C_Slave/I2C_Slave.srcs/sim_1/new/tb_I2C_Slave.sv
+++ I2C_Slave/I2C_Slave.srcs/sim_1/new/tb_I2C_Slave.sv
@@ -35,38 +35,33 @@ module tb_I2C_Slave ();
         for (int i = 0; i < 8; i++) begin
             wait (u_I2C_Slave.state == DATA_CL0);
             master_sda = temp_data[7];
-            repeat (500) @(posedge clk);
-            scl = 1;
+            repeat (500) @(posedge clk) scl = 1;
 
             wait (u_I2C_Slave.state == DATA_CL1);
             bit_count++;
-            repeat (500) @(posedge clk);
-            scl = 0;
+            repeat (500) @(posedge clk) scl = 0;
             temp_data = {temp_data[6:0], 1'b0};
         end
         master_sda = 1'bz;
-        repeat (500) @(posedge clk);
-        scl = 1;
-        repeat (500) @(posedge clk);
-        scl = 0;
+        repeat (500) @(posedge clk) scl = 1;
+        repeat (500) @(posedge clk) scl = 0;
         bit_count = 0;
     endtask  //
 
     task start_transmission();
         // master send start sig
-        wait (u_I2C_Slave.state == IDLE);
-        repeat (500) @(posedge clk); master_sda = 0;
-        wait (u_I2C_Slave.state == START);
-        repeat (500) @(posedge clk); scl = 0;
+        master_sda = 1;
+        scl = 1;
+        repeat (500) @(posedge clk) master_sda = 0;
+        repeat (500) @(posedge clk) scl = 0;
     endtask  //
 
     task end_transmission();
         // master send stop sig
+        scl = 0;
         master_sda = 1'b0;
-        repeat (500) @(posedge clk);
-        scl = 1;
-        repeat (500) @(posedge clk);
-        master_sda = 1;
+        repeat (500) @(posedge clk) scl = 1;
+        repeat (500) @(posedge clk) master_sda = 1;
     endtask  //
 
     always #5 clk = ~clk;
@@ -77,10 +72,11 @@ module tb_I2C_Slave ();
         master_sda = 1;
         scl = 1;
         #10 reset = 0;
-        #25000;
-        
+        repeat (2500) @(posedge clk);
+        $display("test start");
+
         slave_set = 8'b0000_0;
-        word_addr  = 1;
+        word_addr = 1;
 
         start_transmission();
 
@@ -93,7 +89,8 @@ module tb_I2C_Slave ();
 
         end_transmission();
 
-        #25000 $finish;
+        repeat (2500) @(posedge clk);
+        $finish;
     end
 
 endmodule
